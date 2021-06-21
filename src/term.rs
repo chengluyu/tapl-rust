db9851886874712b0d390fa2e476106f30520a91
Luyu Cheng <chengluyu@live.cn>
2021-06-22 03:49:56 +0800

    feat: support subtypes

--- src/term.rs
+++ src/term.rs
@@ -1,12 +1,14 @@
 use crate::{context::Context, termtype::Type};
-use std::fmt::{Display, Formatter, Result as FormatterResult};
+use std::{borrow::Borrow, collections::HashMap, fmt::{Display, Formatter, Result as FormatterResult}};
 
 #[derive(Debug, PartialEq)]
 pub enum Term {
     True,
     False,
     Int(i32),
+    Record(HashMap<String, Term>),
     If(Box<Term>, Box<Term>, Box<Term>),
+    Proj(Box<Term>, String),
     Var(String),
     Abs(String, Box<Type>, Box<Term>),
     App(Box<Term>, Box<Term>),
@@ -18,9 +20,20 @@ impl Display for Term {
             Term::True => write!(f, "true"),
             Term::False => write!(f, "false"),
             Term::Int(value) => write!(f, "{}", value),
+            Term::Record(fields) => {
+                write!(f, "{} ", '{')?;
+                for (index, (field, field_value)) in fields.iter().enumerate() {
+                    write!(f, "{}: {}", field, field_value)?;
+                    if index + 1 < fields.len() {
+                        write!(f, ", ")?;
+                    }
+                }
+                write!(f, " {}", '}')
+            }
             Term::If(test, consequent, alternate) => {
                 write!(f, "if {} then {} else {}", test, consequent, alternate)
             }
+            Term::Proj(target, field) => write!(f, "{}.{}", target, field),
             Term::Var(name) => write!(f, "{}", name),
             Term::Abs(param, param_type, body) => {
                 write!(f, "({}: {:?}) => {}", param, param_type, body)
@@ -38,6 +51,14 @@ impl Term {
             Term::True => Ok(Type::Bool),
             Term::False => Ok(Type::Bool),
             Term::Int(_) => Ok(Type::Int),
+            Term::Record(fields) => {
+                let mut type_fields = HashMap::new();
+                for (field, field_value) in fields {
+                    let field_type = field_value.get_type(context)?;
+                    type_fields.entry(field.clone()).or_insert(field_type);
+                }
+                Ok(Type::Record(type_fields))
+            }
             Term::If(test, consequent, alternate) => match test.get_type(context)? {
                 Type::Bool => {
                     let consequent_type = consequent.get_type(context)?;
@@ -53,6 +74,18 @@ impl Term {
                 }
                 ty => Err(format!("condition should be bool rather than {}", ty)),
             },
+            Term::Proj(target, field) => {
+                let target_type = target.get_type(context)?;
+                if let Type::Record(entries) = &target_type {
+                    if let Some(field_type) = entries.get(field) {
+                        Ok(field_type.clone())
+                    } else {
+                        Err(format!("type {} has no field \"{}\"", target_type, field))
+                    }
+                } else {
+                    Err(format!("expect a record type"))
+                }
+            }
             Term::Var(name) => context.get_type(name.clone()),
             Term::Abs(param, param_type, body_term) => {
                 context.add_binding(param.clone(), *param_type.clone());
@@ -67,7 +100,7 @@ impl Term {
                 let argument_type = argument.get_type(context)?;
                 match callee.get_type(context)? {
                     Type::Arrow(param_type, body_type) => {
-                        if argument_type == *param_type {
+                        if argument_type.is_subtype(param_type.borrow()) {
                             Ok(*body_type.clone())
                         } else {
                             Err(format!(
--- src/termtype.rs
+++ src/termtype.rs
@@ -1,10 +1,15 @@
-use std::fmt::{Display, Formatter, Result};
+use std::{
+    collections::HashMap,
+    fmt::{Display, Formatter, Result},
+};
 
 #[derive(Clone, Debug)]
 pub enum Type {
     Bool,
     Int,
     Arrow(Box<Type>, Box<Type>),
+    Record(HashMap<String, Type>),
+    Top,
 }
 
 impl Type {
@@ -13,6 +18,26 @@ impl Type {
             Type::Arrow(..) => true,
             _ => false,
         }
+    }
+
+    pub fn is_subtype(&self, other: &Type) -> bool {
+        self == other
+            || match (self, other) {
+                (_, Type::Top) => true,
+                (Type::Record(these_fields), Type::Record(those_fields)) => {
+                    those_fields.iter().all(|(that_field, that_field_type)| {
+                        if let Some(this_field_type) = these_fields.get(that_field) {
+                            this_field_type.is_subtype(that_field_type)
+                        } else {
+                            false
+                        }
+                    })
+                }
+                (Type::Arrow(self_left, self_right), Type::Arrow(other_left, other_right)) => {
+                    self_left.is_subtype(other_left) && self_right.is_subtype(other_right)
+                }
+                _ => false,
+            }
     }
 }
 
@@ -25,6 +50,20 @@ impl PartialEq for Type {
                 Type::Arrow(s1, s2) => t1.eq(s1) && t2.eq(s2),
                 _ => false,
             },
+            Type::Record(this_fields) => {
+                if let Type::Record(that_fields) = other {
+                    this_fields.len() == that_fields.len()
+                        && this_fields.iter().all(|(field, this_field_type)| {
+                            that_fields.get(field).map_or_else(
+                                || false,
+                                |that_field_type| this_field_type == that_field_type,
+                            )
+                        })
+                } else {
+                    false
+                }
+            }
+            Type::Top => matches!(other, Type::Top),
         }
     }
 }
@@ -41,6 +80,17 @@ impl Display for Type {
                     write!(f, "{} -> {}", left, right)
                 }
             }
+            Type::Record(fields) => {
+                write!(f, "{} ", '{')?;
+                for (index, (field, field_type)) in fields.iter().enumerate() {
+                    write!(f, "{}: {}", field, field_type)?;
+                    if index + 1 < fields.len() {
+                        write!(f, ", ")?;
+                    }
+                }
+                write!(f, " {}", '}')
+            }
+            Type::Top => write!(f, "⊤"),
         }
     }
 }
@@ -49,6 +99,52 @@ impl Display for Type {
 mod tests {
     use super::*;
     use crate::parser::TermTypeParser;
+
+    #[test]
+    fn test_subtype_top() {
+        assert!(Type::Bool.is_subtype(&Type::Top));
+        assert!(Type::Int.is_subtype(&Type::Top));
+        assert!(Type::Arrow(Box::new(Type::Int), Box::new(Type::Int)).is_subtype(&Type::Top));
+        let mut entries = HashMap::new();
+        entries.entry("x".into()).or_insert(Type::Int);
+        entries.entry("y".into()).or_insert(Type::Int);
+        assert!(Type::Record(entries).is_subtype(&Type::Top));
+    }
+
+    #[test]
+    fn test_subtype_record() {
+        // { x: Int, y: Int } <: { x: Int, y: Int, z: Int }
+        let mut entries = HashMap::new();
+        entries.entry("x".into()).or_insert(Type::Int);
+        entries.entry("y".into()).or_insert(Type::Int);
+        let xiyi = Type::Record(entries);
+        let mut entries = HashMap::new();
+        entries.entry("x".into()).or_insert(Type::Int);
+        entries.entry("y".into()).or_insert(Type::Int);
+        entries.entry("z".into()).or_insert(Type::Int);
+        let xiyizi = Type::Record(entries);
+        assert!(xiyizi.is_subtype(&xiyi));
+        assert!(!xiyi.is_subtype(&xiyizi));
+        // { x: Int, y: Int } <: {}
+        // { x: Int, y: Int, z: Int } <: {}
+        let entries = HashMap::new();
+        let empty = Type::Record(entries);
+        assert!(xiyi.is_subtype(&empty));
+        assert!(xiyizi.is_subtype(&empty));
+        // { x: Int, y: Int } <: { x: Top, y: Top }
+        let mut entries = HashMap::new();
+        entries.entry("x".into()).or_insert(Type::Top);
+        entries.entry("y".into()).or_insert(Type::Top);
+        let xtyt = Type::Record(entries);
+        assert!(xiyi.is_subtype(&xtyt));
+        // { x: Int, y: Int } does nothing with { x: Top, y: Top, z: Top }
+        let mut entries = HashMap::new();
+        entries.entry("x".into()).or_insert(Type::Top);
+        entries.entry("y".into()).or_insert(Type::Top);
+        entries.entry("z".into()).or_insert(Type::Top);
+        let xtytzt = Type::Record(entries);
+        assert!(!xiyi.is_subtype(&xtytzt));
+    }
 
     #[test]
     fn test_parse() {
